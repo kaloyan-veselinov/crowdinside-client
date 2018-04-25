437267bcfe010dada62f974ab2d6910cc5fd0fc7
Kaloyan Veselinov <kvesselinov@gmail.com>
2018-04-25 16:43:04 +0200

    Inform currently logging

--- app/src/main/java/com/kaloyanveselinov/crowdinsideclient/MainActivity.java
+++ app/src/main/java/com/kaloyanveselinov/crowdinsideclient/MainActivity.java
@@ -19,8 +19,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean isLogging = sharedPreferences.getBoolean(getString(R.string.log_key), false);
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
+        informLogging(isLogging, fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,13 +32,7 @@ public class MainActivity extends AppCompatActivity {
                 Boolean isLogging = sharedPreferences.getBoolean(getString(R.string.log_key), false);
                 editor.putBoolean("log", !isLogging);
                 editor.apply();
-                // TODO add sharedPreferences inversion
-                if (!isLogging)
-                    Snackbar.make(view, R.string.logging, Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                else
-                    Snackbar.make(view, R.string.no_logging, Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                informLogging(!isLogging, view);
             }
         });
 
@@ -49,5 +47,10 @@ public class MainActivity extends AppCompatActivity {
 //        mLogger.setLogGPS(sharedPreferences.getBoolean(getString(R.string.log_gps), false));
 //    }
 
-
+    private void informLogging(Boolean isLogging, View view){
+        if (isLogging)
+            Snackbar.make(view, R.string.logging, Snackbar.LENGTH_LONG).show();
+        else
+            Snackbar.make(view, R.string.no_logging, Snackbar.LENGTH_LONG).show();
+    }
 }
